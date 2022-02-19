5ed9508dbd611362c94373367027c0945cb08751
AliOmarTO <87345237+AliOmarTO@users.noreply.github.com>
2022-02-19 18:46:27 -0500

    Update organizer.bat

--- organizer.bat
+++ organizer.bat
@@ -4,7 +4,7 @@ ECHO Would you like to organize files?
 PAUSE
 rem For each file in your folder
 for %%a in (".\*") do (
-rem check if the file has an extension and if it does not, skips the file
+rem check if the file has an extension and if it does not, skips the file. Also checks if the current file is the .bat itself
 if "%%~xa" NEQ "" if "%%~dpxa" NEQ "%~dpx0" (
 rem check if extension folder exists, if not it is created
 if not exist "%%~xa" mkdir "%%~xa"
